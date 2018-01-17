d6cf4bbf2f2594826dae0ce96754c5a9d3553812
Assisneto <netodamasceno12@gmail.com>
2018-01-17 12:14:52 -0200

    complemento da rede multicamada

---
+++ rede-multicamada.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 19 20:38:44 2017
+
+@author: Jones
+"""
+import numpy as np
+
+def sigmoid(soma):
+    return 1 / (1 + np.exp(-soma))
+
+def sigmoidDerivada(sig):
+    return sig * (1 - sig)
+
+#a = sigmoid(0.5)
+#b = sigmoidDerivada(a)
+
+#a = sigmoid(-1.5)
+#b = np.exp(0)
+
+entradas = np.array([[0,0],
+                     [0,1],
+                     [1,0],
+                     [1,1]])
+
+saidas = np.array([[0],[1],[1],[0]])
+
+#pesos0 = np.array([[-0.424, -0.740, -0.961],
+#                   [0.358, -0.577, -0.469]])
+
+#pesos1 = np.array([[-0.017], [-0.893], [0.148]])
+
+pesos0 = 2*np.random.random((2,3)) - 1
+pesos1 = 2*np.random.random((3,1)) - 1
+
+epocas = 1000000
+taxaAprendizagem = 0.6
+momento = 1
+
+for j in range(epocas):
+    camadaEntrada = entradas
+    somaSinapse0 = np.dot(camadaEntrada, pesos0)
+    camadaOculta = sigmoid(somaSinapse0)
+
+    somaSinapse1 = np.dot(camadaOculta, pesos1)
+    camadaSaida = sigmoid(somaSinapse1)
+
+    erroCamadaSaida = saidas - camadaSaida
+    mediaAbsoluta = np.mean(np.abs(erroCamadaSaida))
+    print("Erro: " + str(mediaAbsoluta))
+
+    derivadaSaida = sigmoidDerivada(camadaSaida)
+    deltaSaida = erroCamadaSaida * derivadaSaida
+
+    pesos1Transposta = pesos1.T
+    deltaSaidaXPeso = deltaSaida.dot(pesos1Transposta)
+    deltaCamadaOculta = deltaSaidaXPeso * sigmoidDerivada(camadaOculta)
+
+    camadaOcultaTransposta = camadaOculta.T
+    pesosNovo1 = camadaOcultaTransposta.dot(deltaSaida)
+    pesos1 = (pesos1 * momento) + (pesosNovo1 * taxaAprendizagem)
+
+    camadaEntradaTransposta = camadaEntrada.T
+    pesosNovo0 = camadaEntradaTransposta.dot(deltaCamadaOculta)
+    pesos0 = (pesos0 * momento) + (pesosNovo0 * taxaAprendizagem)
